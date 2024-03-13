0a8e2975042ee6edc1f913df953fc897976d894f
Harsh Vardhan Mishra <138967484+HARSH-VARDHAN-MISHRA@users.noreply.github.com>
2024-03-13 10:16:47 +0530

    first commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carasol from '../../components/carasol/Carasol'
+
+const Home = () => {
+  return (
+    <div>
+      <Carasol/>
+    </div>
+  )
+}
+
+export default Home
